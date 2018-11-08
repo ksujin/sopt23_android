25283ef3cceaabad1c15ac6b0afc381c66bec72c
강수진 <rkdthd1234@naver.com>
2018-11-08 12:37:57 +0900

    2차과제

--- 2차/sopt23_assignment_02/app/src/main/java/com/example/kangsujin/sopt23_assignment_02/MainActivity.kt
+++ 2차/sopt23_assignment_02/app/src/main/java/com/example/kangsujin/sopt23_assignment_02/MainActivity.kt
@@ -4,18 +4,51 @@ import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivityForResult
 
 class MainActivity : AppCompatActivity() {
+    fun hideSoftKeyboard(activity: Activity) {
+        val inputMethodManager = activity.getSystemService(
+                Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager!!.hideSoftInputFromWindow(
+                activity.currentFocus!!.windowToken, 0)
+    }
+
+    fun setupUI(view: View) {
+
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (view !is EditText) {
+            view.setOnTouchListener(object : View.OnTouchListener {
+                override fun onTouch(v: View, event: MotionEvent): Boolean {
+                    hideSoftKeyboard(this@MainActivity)
+                    return false
+                }
+            })
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view is ViewGroup) {
+            for (i in 0 until (view as ViewGroup).childCount) {
+                val innerView = (view as ViewGroup).getChildAt(i)
+                setupUI(innerView)
+            }
+        }
+    }
 
     val REQUEST_CODE_SIGN_IN_ACTIVITY = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupUI(findViewById(R.id.view_main_act_parent))
         btn_main_act_sign_in.setOnClickListener {
-            startActivityForResult<SignInActivity>(REQUEST_CODE_SIGN_IN_ACTIVITY, "id" to tv_main_act_id.text)
+            startActivityForResult<SignInActivity>(REQUEST_CODE_SIGN_IN_ACTIVITY, "id" to et_main_act_id.text.toString(), "pw" to et_main_act_pw.text.toString())
         }
 
     }
@@ -28,6 +61,9 @@ class MainActivity : AppCompatActivity() {
             if (resultCode == Activity.RESULT_OK){
                 data?.getStringExtra("editedId").let {
                     et_main_act_id.setText(it)
+                }
+                data?.getStringExtra("editedPw").let {
+                    et_main_act_pw.setText(it)
                 }
             }
         }
--- 2차/sopt23_assignment_02/app/src/main/java/com/example/kangsujin/sopt23_assignment_02/SignInActivity.kt
+++ 2차/sopt23_assignment_02/app/src/main/java/com/example/kangsujin/sopt23_assignment_02/SignInActivity.kt
@@ -4,20 +4,55 @@ import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
+    fun hideSoftKeyboard(activity: Activity) {
+        val inputMethodManager = activity.getSystemService(
+                Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager!!.hideSoftInputFromWindow(
+                activity.currentFocus!!.windowToken, 0)
+    }
+
+    fun setupUI(view: View) {
+
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (view !is EditText) {
+            view.setOnTouchListener(object : View.OnTouchListener {
+                override fun onTouch(v: View, event: MotionEvent): Boolean {
+                    hideSoftKeyboard(this@SignInActivity)
+                    return false
+                }
+            })
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view is ViewGroup) {
+            for (i in 0 until (view as ViewGroup).childCount) {
+                val innerView = (view as ViewGroup).getChildAt(i)
+                setupUI(innerView)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
         et_sign_in_act_id.setText(intent.getStringExtra("id"))
+        et_sign_in_act_pw.setText(intent.getStringExtra("pw"))
+        setupUI(findViewById(R.id.view_sign_in_act_parent))
         btn_sign_in_act_complete.setOnClickListener{
             (it as Button).text = "complete"
             val intent : Intent = Intent()
-            intent.putExtra("editedId", et_sign_in_act_id.text)
+            intent.putExtra("editedId", et_sign_in_act_id.text.toString())
+            intent.putExtra("editedPw", et_sign_in_act_pw.text.toString())
             setResult(Activity.RESULT_OK, intent)
             finish()
         }
